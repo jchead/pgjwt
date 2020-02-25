5fb24e6e6fa8e12610251f500cb5a6633dc695da
John Head <jcheadiv@gmail.com>
2020-02-25 05:16:18 +0000

    Added testing for postgresql 9-11

--- test.sh
+++ test.sh
@@ -7,8 +7,75 @@ DB_NAME="postgres"
 SU="postgres"
 EXEC="docker exec $DB_HOST"
 
-echo building test image
-docker build . --force-rm -t pgjwt/test
+
+echo building test postgres 12 image
+docker build . -f Dockerfile --force-rm -t pgjwt/test
+
+echo running test container
+docker run -d -e POSTGRES_PASSWORD=password --name "$DB_HOST" pgjwt/test
+
+echo waiting for database to accept connections
+until
+    $EXEC \
+	    psql -o /dev/null -t -q -U "$SU" \
+        -c 'select pg_sleep(1)' \
+	    2>/dev/null;
+do sleep 1;
+done
+
+echo running tests
+$EXEC pg_prove -U "$SU" /pgjwt/test.sql
+
+echo destroying test container and image
+docker rm --force "$DB_HOST"
+docker rmi pgjwt/test
+
+echo building test postgres 11 image
+docker build . -f Dockerfile.11 --force-rm -t pgjwt/test
+
+echo running test container
+docker run -d -e POSTGRES_PASSWORD=password --name "$DB_HOST" pgjwt/test
+
+echo waiting for database to accept connections
+until
+    $EXEC \
+	    psql -o /dev/null -t -q -U "$SU" \
+        -c 'select pg_sleep(1)' \
+	    2>/dev/null;
+do sleep 1;
+done
+
+echo running tests
+$EXEC pg_prove -U "$SU" /pgjwt/test.sql
+
+echo destroying test container and image
+docker rm --force "$DB_HOST"
+docker rmi pgjwt/test
+
+echo building test postgres 10 image
+docker build . -f Dockerfile.10 --force-rm -t pgjwt/test
+
+echo running test container
+docker run -d -e POSTGRES_PASSWORD=password --name "$DB_HOST" pgjwt/test
+
+echo waiting for database to accept connections
+until
+    $EXEC \
+	    psql -o /dev/null -t -q -U "$SU" \
+        -c 'select pg_sleep(1)' \
+	    2>/dev/null;
+do sleep 1;
+done
+
+echo running tests
+$EXEC pg_prove -U "$SU" /pgjwt/test.sql
+
+echo destroying test container and image
+docker rm --force "$DB_HOST"
+docker rmi pgjwt/test
+
+echo building test postgres 9 image
+docker build . -f Dockerfile.9 --force-rm -t pgjwt/test
 
 echo running test container
 docker run -d -e POSTGRES_PASSWORD=password --name "$DB_HOST" pgjwt/test
